41498a77e64ae4a94149e7a4e4d52b00474238c8
deadend135 <sharma.vikrant135@gmail.com>
2020-05-03 21:15:53 +0530

    Adding game features hibt and validation

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import React, { useState, useRef } from "react";
+import { StyleSheet, View, Text, Button, Alert } from "react-native";
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
 
@@ -19,31 +19,66 @@ const GameScreen = (props) => {
     generateRandomBetween(1, 100, props.userChoice)
   );
 
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie", "You know that this is wrong ....", [
+        { text: "Sorry", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <Card style = {styles.buttonContainer}>
-          <Button style={styles.buttonContainer} title="LOWER" onPress={() => {}}/>
-          <Button style={styles.buttonContainer} title="GREATER" onPress={() => {}}/>
+      <Card style={styles.buttonContainer}>
+        <Button
+          style={styles.buttonContainer}
+          title="LOWER"
+          onPress={nextGuessHandler.bind(this, "lower")}
+        />
+        <Button
+          style={styles.buttonContainer}
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, "greater")}
+        />
       </Card>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        padding:10,
-        alignItems:'center'
-    },
-    buttonContainer:{
-        flexDirection:'row',
-        justifyContent:'space-around',
-        marginTop:20,
-        width:'80%'  
-         },
-    
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: "80%",
+  },
 });
 
 export default GameScreen;
